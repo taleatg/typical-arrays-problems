01decccd409292590ab80b7b6a12802e21f566c2
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-09-30 17:49:04 +0400

    perform functions: max., min. and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
+let res = 0;
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array || !array.length) return 0;
+
+  res = array[0];
+    for (var i = 0; i < array.length; i++) { 
+    if (res > array[i]) {
+      res = array[i]; 
+        } 
+    }
+    return res;
+  } 
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array || !array.length) return 0;
+
+   res = array[0];
+    for (var i = 0; i < array.length; i++) { 
+        if (res < array[i]) {
+          res = array[i]; 
+        }
+    }
+    return res;
+  }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  
+  let sum = 0;
+  res = array[0]
+  for (i=0; i < array.length; i++){
+    sum += array[i];
+  }
+    res = sum / array.length;
+ 
+  return res;
 }
